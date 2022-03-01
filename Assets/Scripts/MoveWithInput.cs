c7e28fa1e42eac32ee756a503c636ef0ce45c55b
HAQUE <S6061420@live.tees.ac.uk>
2022-03-01 13:46:34 +0000

    added ui and input for where to kick the ball

---
+++ Assets/Scripts/MoveWithInput.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithInput : MonoBehaviour
+{
+    private Vector3 _parentPos;
+    private Rect _parentRect;
+    private readonly float _kMovementStepSize = 1f;
+    private Vector3 _newPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _parentPos = transform.parent.position;
+        _parentRect = transform.parent.GetComponent<RectTransform>().rect;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessHorizontalInput();
+        ProcessVerticalInput();
+    }
+
+    private void ProcessHorizontalInput()
+    {
+        if (Input.GetAxis("HorizontalRightAnalogStick") > 0.5)
+        {
+            _newPos = new Vector3(transform.position.x + _kMovementStepSize, transform.position.y, 0);
+            if (IsPointWithinBallUI(_newPos))
+            {
+                transform.position = _newPos;
+            }
+        }
+        else if (Input.GetAxis("HorizontalRightAnalogStick") < -0.5)
+        {
+            _newPos = new Vector3(transform.position.x - _kMovementStepSize, transform.position.y, 0);
+            if (IsPointWithinBallUI(_newPos))
+            {
+                transform.position = _newPos;
+            }
+        }
+    }
+
+    private void ProcessVerticalInput()
+    {
+        if (Input.GetAxis("VerticalRightAnalogStick") > 0.5)
+        {
+            _newPos = new Vector3(transform.position.x, transform.position.y + _kMovementStepSize, 0);
+            if (IsPointWithinBallUI(_newPos))
+            {
+                transform.position = _newPos;
+            }
+        }
+        else if (Input.GetAxis("VerticalRightAnalogStick") < -0.5)
+        {
+            _newPos = new Vector3(transform.position.x, transform.position.y - _kMovementStepSize, 0);
+            if (IsPointWithinBallUI(_newPos))
+            {
+                transform.position = _newPos;
+            }
+        }
+    }
+
+    private bool IsPointWithinBallUI(Vector3 point)
+    {
+        float distance = Vector3.Distance(point, _parentPos);
+
+        //width of image / 2 = radius of ball
+        if (distance < _parentRect.width / 2)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
