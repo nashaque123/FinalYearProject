bf5974a4ebfbdc17597265ac075ed273be44c62c
HAQUE <S6061420@live.tees.ac.uk>
2022-03-26 19:48:38 +0000

    added drag to velocity

--- Assets/Scripts/AdamsMoultonSolver.cs
+++ Assets/Scripts/AdamsMoultonSolver.cs
@@ -8,11 +8,14 @@ public class AdamsMoultonSolver : MonoBehaviour
     private readonly float kGravity = -0.1635f; // 9.81 / 60fps
     private float _posX, _posY, _posZ;
     private MagnusForce magnusForce;
+    private readonly float _dragCoefficient = 0.25f;
     public BooleanScriptableObject ShotTaken;
     public BooleanScriptableObject BallInMotion;
     public GameObject AimArrow;
     public Transform Cursor;
     private Transform _ballUI;
+    public Air Air;
+    private readonly float kPi = 3.1415f;
 
     // Start is called before the first frame update
     void Start()
@@ -48,7 +51,9 @@ public class AdamsMoultonSolver : MonoBehaviour
     {
         Vector3 force = magnusForce.CalculateMagnusForce();
         Vector3 magnusAcceleration = force / _ball.Mass;
-        _ball.LinearVelocity = 0.5f * new Vector3(_ball.LinearVelocity.x + _ball.LinearVelocity.x + magnusAcceleration.x, _ball.LinearVelocity.y + _ball.LinearVelocity.y + kGravity + magnusAcceleration.y, _ball.LinearVelocity.z + _ball.LinearVelocity.z + magnusAcceleration.z);
+        float dragFactor = 0.5f * Air.Density * kPi * _ball.Radius * _ball.Radius * _dragCoefficient;
+        Vector3 drag = new Vector3(dragFactor * _ball.LinearVelocity.x * _ball.LinearVelocity.x, dragFactor * _ball.LinearVelocity.y * _ball.LinearVelocity.y, dragFactor * _ball.LinearVelocity.z * _ball.LinearVelocity.z);
+        _ball.LinearVelocity = 0.5f * new Vector3(_ball.LinearVelocity.x + _ball.LinearVelocity.x + magnusAcceleration.x + drag.x, _ball.LinearVelocity.y + _ball.LinearVelocity.y + kGravity + magnusAcceleration.y + drag.y, _ball.LinearVelocity.z + _ball.LinearVelocity.z + magnusAcceleration.z + drag.z);
 
         _posX = transform.position.x + _ball.LinearVelocity.x;
         _posY = transform.position.y + _ball.LinearVelocity.y;
