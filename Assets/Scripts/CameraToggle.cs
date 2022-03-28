b2f3d3a8f4e13daac10ff63600ba82c7ca7b94ee
HAQUE <S6061420@live.tees.ac.uk>
2022-03-28 15:57:47 +0100

    added camera toggle

---
+++ Assets/Scripts/CameraToggle.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraToggle : MonoBehaviour
+{
+    public Camera[] Cameras = new Camera[3];
+    private int _cameraIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetCameraEnabled();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("joystick button 3") || Input.GetKeyDown("y"))
+        {
+            _cameraIndex++;
+            _cameraIndex %= Cameras.Length;
+            SetCameraEnabled();
+        }
+    }
+
+    private void SetCameraEnabled()
+    {
+        for (int i = 0; i < Cameras.Length; i++)
+        {
+            Cameras[i].enabled = i == _cameraIndex;
+        }
+    }
+}
