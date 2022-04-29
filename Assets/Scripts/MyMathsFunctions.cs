4c5e4f4e52d31b7ea829379bcbafc7faf92c6f32
HAQUE <S6061420@live.tees.ac.uk>
2022-04-29 15:35:20 +0100

    fixed collision with posts going through net bug

--- Assets/Scripts/MyMathsFunctions.cs
+++ Assets/Scripts/MyMathsFunctions.cs
@@ -33,4 +33,13 @@ public static class MyMathsFunctions
         float cosAngle = CalculateCosAngleInRadiansBetweenVectors(v1, v2);
         return Mathf.Acos(cosAngle);
     }
+
+    public static Vector3 CalculateCrossProduct(Vector3 v1, Vector3 v2)
+    {
+        float x = (v1.y * v2.z) - (v1.z * v2.y);
+        float y = (-v1.x * v2.z) + (v1.z * v2.x);
+        float z = (v1.x * v2.y) - (v1.y * v2.x);
+
+        return new Vector3(x, y, z);
+    }
 }
